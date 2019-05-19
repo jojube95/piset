06accebc004ab38e61a283eed1fdd784783fb9eb
jojube95 <joanjube95@gmail.com>
2019-05-19 15:37:53 +0200

    Refactor groupDAO.

--- src/app/dao/group-storage.service.ts
+++ src/app/dao/group-storage.service.ts
@@ -11,39 +11,21 @@ import {UserModel} from '../model/userModel';
   providedIn: 'root'
 })
 export class GroupStorageService {
-  groupsRef: AngularFireList<Group>;
-
-  groupsObservable: Observable<Group[]>;
-
   constructor(private af: AngularFireDatabase, private afAuth: AngularFireAuth, private userStorage: UserStorageService) {
-    this.groupsRef = this.af.list('groups');
-    this.groupsObservable = this.groupsRef.snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
-      )
-    );
+   
   }
 
-  getObservableGroups(){
-    return this.groupsObservable;
+  getGroups(){
+    return <Observable<Group[]>>this.af.list('groups').valueChanges();
   }
-
-  getGroupsFireList(){
-    return this.groupsRef;
-  }
-
-  getCurrentUserGroup(){
-    return null;
-  }
+ 
 
   updateGroup(group: Group){
     this.af.object('groups/' + group.key).update(group);
   }
 
   createGroup(groupObj: Group){
-    groupObj.users.push(new UserModel('test@test.com', 'testtest', 'testname', 'testsecond', false));
-    groupObj.users.push(new UserModel('test2@test.com', 'testtest2', 'testname2', 'testsecond2', false));
-    this.groupsRef.push(groupObj);
+    this.af.list('groups').push(groupObj);
   }
 
   deleteGroup(group: Group){
@@ -55,7 +37,7 @@ export class GroupStorageService {
   }
 
   getUsersFromGroup(group: Group) {
-    return this.af.list('groups/' + group.key + '/users');
+    return this.af.list('groups/' + group.key + '/users').valueChanges();
   }
 
 
