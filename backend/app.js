dc4a8333ae97f96f12ee263ff0cdf930c06c0562
jojube95 <joanjube95@gmail.com>
2019-12-24 11:55:34 +0100

    Sheduled task for shuffle tasks.

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,9 @@ const tasksRoutes = require('./routes/tasks');
 const subtasksRoutes = require('./routes/subtasks');
 const penaltiesRoutes = require('./routes/penalties');
 const io = require('socket.io-client');
-
+const cron = require('node-cron');
 const app  = express();
+const request = require('request');
 
 mongoose.connect('mongodb+srv://root:root@cluster0-53xnf.mongodb.net/piset', {useNewUrlParser: true, useUnifiedTopology: true}).then( () => {
   console.log('Connected to database!');
@@ -32,12 +33,43 @@ app.use((req, res, next) => {
   next();
 });
 
+cron.schedule("59 23 * * SUN", function () {
+  let currentdate = new Date();
+  let datetime = "Last Sync: " + currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/"
+    + currentdate.getFullYear() + " @ "
+    + currentdate.getHours() + ":"
+    + currentdate.getMinutes() + ":"
+    + currentdate.getSeconds();
+
+  console.log("Running Cron Job at: " + datetime);
+  //Do something
+  //Get all groups
+
+  //Do this for all groups
+    let options = {
+      uri: 'http://localhost:3000/api/tasks/reasign',
+      method: 'POST',
+      json: data.group
+    };
+    request.post(options, function (error, response, body) {
+      if (!error) {
+        console.log('Task sheduled for group: ' + groupId);
+      }
+      else{
+        console.log('ERROR Task sheduled for group: ' + groupId);
+      }
+    });
+
+});
+
 const socketServer = io('http://localhost:5000');
 
 app.use(function(req, res, next) {
   req.socketServer = socketServer;
   next();
 });
+
 
 
 app.use('/api/users', usersRoutes);
