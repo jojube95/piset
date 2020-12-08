860cf406560365221d6354ecfb955ae26a525875
jojube95 <joanjube95@gmail.com>
2020-12-08 19:52:24 +0100

    Delete all files before pull piset-ionic project

--- backend/models/group.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = require ('../models/user').schema;
-
-const groupSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  users: [{type: userSchema, required: false}]
-});
-
-module.exports = mongoose.model('Group', groupSchema);
