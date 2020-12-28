07996056051a6cf87e7e410aacc4785efb564b29
jojube95 <joanjube95@gmail.com>
2020-12-28 17:32:43 +0100

    Separate backend/database from this project

--- backend/models/group.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = require ('../models/user').schema;
-
-const groupSchema = mongoose.Schema({
-  name: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Group', groupSchema);
