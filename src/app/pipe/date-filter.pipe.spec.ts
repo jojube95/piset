1a1f648cfafd6d90c6cb3c78b26162121b07d4ac
jojube95 <joanjube95@gmail.com>
2021-03-19 13:20:56 +0100

    Unit tests

--- src/app/pipe/date-filter.pipe.spec.ts
+++ src/app/pipe/date-filter.pipe.spec.ts
@@ -1,94 +1,76 @@
 import { DateFilterPipe } from './date-filter.pipe';
 import { History} from '../model/history';
 
-xdescribe('DateFilterPipe', () => {
+describe('DateFilterPipe', () => {
   let list1, list2, list3, list4, list5, list6;
   let history1, history2, history3, history4;
-  let date1, date2, date3, date4, date5, date6;
   let datefilter1, datefilter2, datefilter3, datefilter4, datefilter5;
   let pipe = new DateFilterPipe();
-  beforeEach(()=> {
-    date1 = new Date(Date.UTC(2020, 0, 0));
-    date2 = new Date(Date.UTC(2020, 0, 28));
-    date3 = new Date(Date.UTC(2020, 1, 1));
-    date4 = new Date(Date.UTC(2020, 1, 28));
-    date5 = new Date(Date.UTC(2020, 2, 3));
-    date6 = new Date(Date.UTC(2020, 2, 25));
-
-    datefilter1 = new Date(Date.UTC(2020, 0, 0));
-    datefilter2 = new Date(Date.UTC(2020, 1, 0));
-    datefilter3 = new Date(Date.UTC(2020, 2, 0));
-    datefilter4 = new Date(Date.UTC(2020, 3, 0));
-    datefilter5 = new Date(Date.UTC(2021, 0, 0));
-
-    history1 = new History('1', 'SubtaskName1', 1, true, '1', 'User1', date1, date2);
-    history2 = new History('2', 'SubtaskName2', 1, true, '2', 'User2', date3, date4);
-    history3 = new History('3', 'SubtaskName3', 1, true, '3', 'User3', date5, date6);
-    history4 = new History('4', 'SubtaskName4', 1, true, '4', 'User4', date2, date3);
-
-    list1 = [];
-
-    list2 = [];
-
-    list2.push(history1);
-
-    list3 = [];
-
-    list3.push(history1);
-    list3.push(history2);
-
-    list4 = [];
-
-    list4.push(history1);
-    list4.push(history2);
-    list4.push(history3);
-
-    list5 = [];
-
-    list5.push(history2);
-    list5.push(history3);
-
-    list6 = [];
-
-    list6.push(history1);
-    list6.push(history3);
-    list6.push(history4);
-
-  });
 
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
   it('should filter data', () => {
-    expect(pipe.transform(list1, datefilter1)).toEqual([]);
+    datefilter1 = new Date(Date.UTC(2020, 0, 1));
+    datefilter2 = new Date(Date.UTC(2020, 1, 1));
+    datefilter3 = new Date(Date.UTC(2020, 2, 1));
+    datefilter4 = new Date(Date.UTC(2020, 3, 1));
+    datefilter5 = new Date(Date.UTC(2021, 0, 1));
 
-    expect(pipe.transform(list2, datefilter1)).toEqual([]);
-    expect(pipe.transform(list2, datefilter2)).toContain(history1);
-    expect(pipe.transform(list2, datefilter5)).toEqual([]);
+    history1 = new History('1', 'Subtask1', 1, true, '1', 'User1', new Date(Date.UTC(2020, 0, 1)), new Date(Date.UTC(2020, 0, 28)));
+    history2 = new History('2', 'Subtask2', 1, true, '2', 'User2', new Date(Date.UTC(2020, 1, 1)), new Date(Date.UTC(2020, 1, 28)));
+    history3 = new History('3', 'Subtask3', 1, true, '3', 'User3', new Date(Date.UTC(2020, 2, 3)), new Date(Date.UTC(2020, 2, 25)));
+    history4 = new History('4', 'Subtask4', 1, true, '4', 'User4', new Date(Date.UTC(2020, 0, 28)), new Date(Date.UTC(2020, 1, 1)));
 
-    expect(pipe.transform(list3, datefilter1)).toContain(history1);
-    expect(pipe.transform(list3, datefilter2)).toContain(history2);
-    expect(pipe.transform(list3, datefilter3)).toEqual([]);
-    expect(pipe.transform(list3, datefilter5)).toEqual([]);
+    list1 = [];
 
-    expect(pipe.transform(list4, datefilter1)).toContain(history1);
-    expect(pipe.transform(list4, datefilter2)).toContain(history2);
-    expect(pipe.transform(list4, datefilter3)).toContain(history3);
-    expect(pipe.transform(list4, datefilter4)).toEqual([]);
-    expect(pipe.transform(list4, datefilter5)).toEqual([]);
+    list2 = [history1];
 
-    expect(pipe.transform(list5, datefilter1)).toEqual([]);
-    expect(pipe.transform(list5, datefilter2)).toContain(history2);
-    expect(pipe.transform(list5, datefilter3)).toContain(history3);
-    expect(pipe.transform(list5, datefilter4)).toEqual([]);
-    expect(pipe.transform(list5, datefilter5)).toEqual([]);
+    list3 = [history1, history2];
 
-    expect(pipe.transform(list6, datefilter1)).toContain(history4);
-    expect(pipe.transform(list6, datefilter1)).toContain(history1);
-    expect(pipe.transform(list6, datefilter2)).toContain(history4);
-    expect(pipe.transform(list6, datefilter3)).toContain(history3);
-    expect(pipe.transform(list6, datefilter4)).toEqual([]);
-    expect(pipe.transform(list6, datefilter5)).toEqual([]);
+    list4 = [history1, history2, history3];
+
+    list5 = [history2, history3];
+
+    list6 = [history1, history3, history4];
+
+    expect(pipe.transform(list1, [datefilter1])).toEqual([]);
+    expect(pipe.transform(list1, [datefilter2])).toEqual([]);
+    expect(pipe.transform(list1, [datefilter3])).toEqual([]);
+    expect(pipe.transform(list1, [datefilter4])).toEqual([]);
+    expect(pipe.transform(list1, [datefilter5])).toEqual([]);
+
+    expect(pipe.transform(list2, [datefilter1])).toContain(history1);
+    expect(pipe.transform(list2, [datefilter2])).toEqual([]);
+    expect(pipe.transform(list2, [datefilter3])).toEqual([]);
+    expect(pipe.transform(list2, [datefilter4])).toEqual([]);
+    expect(pipe.transform(list2, [datefilter5])).toEqual([]);
+
+
+    expect(pipe.transform(list3, [datefilter1])).toContain(history1);
+    expect(pipe.transform(list3, [datefilter2])).toContain(history2);
+    expect(pipe.transform(list3, [datefilter3])).toEqual([]);
+    expect(pipe.transform(list3, [datefilter4])).toEqual([]);
+    expect(pipe.transform(list3, [datefilter5])).toEqual([]);
+
+    expect(pipe.transform(list4, [datefilter1])).toContain(history1);
+    expect(pipe.transform(list4, [datefilter2])).toContain(history2);
+    expect(pipe.transform(list4, [datefilter3])).toContain(history3);
+    expect(pipe.transform(list4, [datefilter4])).toEqual([]);
+    expect(pipe.transform(list4, [datefilter5])).toEqual([]);
+
+    expect(pipe.transform(list5, [datefilter1])).toEqual([]);
+    expect(pipe.transform(list5, [datefilter2])).toContain(history2);
+    expect(pipe.transform(list5, [datefilter3])).toContain(history3);
+    expect(pipe.transform(list5, [datefilter4])).toEqual([]);
+    expect(pipe.transform(list5, [datefilter5])).toEqual([]);
+
+    expect(pipe.transform(list6, [datefilter1])).toContain(history4);
+    expect(pipe.transform(list6, [datefilter1])).toContain(history1);
+    expect(pipe.transform(list6, [datefilter2])).toContain(history4);
+    expect(pipe.transform(list6, [datefilter3])).toContain(history3);
+    expect(pipe.transform(list6, [datefilter4])).toEqual([]);
+    expect(pipe.transform(list6, [datefilter5])).toEqual([]);
   });
 });
